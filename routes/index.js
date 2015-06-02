2dc6e6ab9ea505ed74ef7827ef6c6ef05603696f
hmarouan <marouan.harrouch@gmail.com>
2015-06-02 19:53:21 +0100

    Signed-off-by: hmarouan <marouan.harrouch@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var mongo = require('mongodb');
 var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
- 
+
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 
 // create cartdb Database
